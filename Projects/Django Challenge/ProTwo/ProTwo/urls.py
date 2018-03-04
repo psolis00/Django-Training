1e455d4deb7a9e320668bcb68b2fd23fcf2d0e52
Patrick Solis <psolis00@gmail.com>
2018-03-04 15:13:54 -0800

    learned django basics

--- Projects/Django Challenge/ProTwo/ProTwo/urls.py
+++ Projects/Django Challenge/ProTwo/ProTwo/urls.py
@@ -1,4 +1,4 @@
-"""first_project URL Configuration
+"""ProTwo URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
@@ -16,9 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
-from first_app import views
+from AppTwo import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.index, name="index"),
     path('admin/', admin.site.urls),
 ]
