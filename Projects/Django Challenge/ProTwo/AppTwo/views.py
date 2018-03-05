eab8357aaa81ce7a9e8224a82aa92d25b80c2a31
Patrick Solis <psolis00@gmail.com>
2018-03-04 23:01:09 -0800

    started url mapping

--- Projects/Django Challenge/ProTwo/AppTwo/views.py
+++ Projects/Django Challenge/ProTwo/AppTwo/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<em>My second app</em>")
+    my_dict = {'insert_help':"Help is on the way!",}
+    return render(request, 'index/help.html', context=my_dict)
 
 # Create your views here.
