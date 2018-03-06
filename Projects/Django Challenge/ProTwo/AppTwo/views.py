f239507c7aa1d939d40dae19ffe2fd54a70b1a36
Patrick Solis <psolis00@gmail.com>
2018-03-05 22:25:15 -0800

    finished Django level one

--- Projects/Django Challenge/ProTwo/AppTwo/views.py
+++ Projects/Django Challenge/ProTwo/AppTwo/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+# Create your views here.
 
 def index(request):
-    my_dict = {'insert_help':"Help is on the way!",}
+    return HttpResponse("<em>My Second Project</em>")
+
+def help(request):
+    my_dict = {'insert_help':"Help is on the way!"}
     return render(request, 'index/help.html', context=my_dict)
 
-# Create your views here.
+
