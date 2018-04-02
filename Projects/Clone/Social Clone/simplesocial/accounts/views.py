d3689b3436cf0a999ddd8b3d92647a40e9a98b44
Patrick Solis <psolis00@gmail.com>
2018-04-01 19:56:42 -0700

    started templates

--- Projects/Clone/Social Clone/simplesocial/accounts/views.py
+++ Projects/Clone/Social Clone/simplesocial/accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from . import forms
+
+from django.views.generic import CreateView
 
 # Create your views here.
+
+class SignUp(CreateView):
+    pass
